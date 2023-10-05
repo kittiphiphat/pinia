e1f0803bc4281ef729298f9053742a6e8facc386
kittiphiphat <makuth824@gmail.com>
2023-10-05 21:22:06 +0700

    pinia

---
+++ src/components/Header.vue
@@ -0,0 +1,31 @@
+<template>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <nav class="navbar navbar-expand-lg rounded navbar-dark bg-dark">
+        <div class="container-fluid">
+            <router-link class="navbar-brand" to="/">mimishop</router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link" aria-current="page" to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link" to="/cart"><i class="fa-solid fa-cart-shopping"></i> {{data.countCartItems}}</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+    import { useShoppingStore } from '../stores'
+    //get store
+    const data = useShoppingStore();
+</script>
+
+<style>
+
+</style>
